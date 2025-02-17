978eea50156ec3ea5ee1ab73c9afaf967e0aea51
Melvin-KJ <melvinofficial2001@gmail.com>
2025-02-17 11:53:09 +0530

    Create CategoryPieChart component for displaying expense categories

--- finance-frontend/src/pages/Dashboard.jsx
+++ finance-frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import StatCard from '@/components/dashboard/StatCard'
 import ExpenseBarChart from '@/components/dashboard/Charts/ExpenseBarChart'
+import CategoryPieChart from '@/components/dashboard/Charts/CategoryPieChart'
 
 const Dashbaord = () => {
 
+    // data objects like {expenseData, categoryData, budgets, expenses}
   
   return (
     <div>
@@ -11,7 +13,7 @@ const Dashbaord = () => {
       <StatCard title="Total Income" amount={12000} type="income"/>
       <div className='grid grid-cols-2 gap-6'>
         {/* <ExpenseBarChart data={expenseData.monthly}/> */}
-        
+        {/* <CategoryPieChart data={categoryData} /> */}
       </div>
     </div>
   )
