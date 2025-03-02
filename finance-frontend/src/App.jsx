24b2a9943ab62161b61b0a76e849fe0fda965e2f
Melvin-KJ <melvinofficial2001@gmail.com>
2025-03-03 05:16:59 +0530

    fix: typo issue coorected

--- finance-frontend/src/App.jsx
+++ finance-frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
 import Login from './pages/Auth/Login';
 import SignUp from './pages/Auth/SignUp';
-import Dashbaord from './pages/Dashboard';
+import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
 import axios from 'axios';
 import { Toaster } from 'react-hot-toast';
 import { UserContextProvider } from './context/userContext';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'http://localhost:3000/api';
 axios.defaults.withCredentials = true;
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashbaord />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </UserContextProvider>
   );
